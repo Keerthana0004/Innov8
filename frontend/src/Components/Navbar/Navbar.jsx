94ac4d87f6694e9050ecf69362273a67b485853a
Keerthana0004 <keerthanasivan04@gmail.com>
2025-01-26 05:08:03 +0530

    search

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -5,9 +5,13 @@ import { Link } from 'react-router-dom';
 import './Navbar.css';
 import logo from '../Assets/logo.jpg';
 import cart from '../Assets/cart.png';
+import search from '../Assets/search.png';
 const Navbar = () => {
 
     const [menu,setMenu] = useState("shop");
+    const handleImageUpload =(file) =>{
+        console.log("Uploaded file:",file);
+    };
     return (
         <div className='navbar'>
 
@@ -22,7 +26,21 @@ const Navbar = () => {
                 <li onClick={()=>{setMenu("women")}}><Link to='/women' style={{ textDecoration: 'none'}}>Women </Link>{menu==="women"?<hr/>:<></>}</li>
                 <li onClick={()=>{setMenu("kids")}}><Link to='/kids' style={{ textDecoration: 'none'}}>Kids </Link> {menu==="kids"?<hr/>:<></>}</li>
             </ul>
-            <div className="nav-login-cart">        
+           
+                
+                <div className="file-upload">
+                <img src={search} alt="" style={{ width: '70px', height: 'auto'}}/>
+                <label htmlFor="file-upload">
+                        <button className="upload-btn">Search by Image</button>
+                    </label>
+                    <input
+                        type="file"
+                        id="file-upload"
+                        style={{ display: 'none' }}
+                        onChange={handleImageUpload}
+                    />
+                </div>
+                <div className="nav-login-cart">        
                 <Link to='/login'><button>Login</button></Link>
                 <Link to='/cart'><img src={cart} alt="" style={{ width: '70px', height: 'auto'}}/></Link>
                 <div className="nav-cart-count">
