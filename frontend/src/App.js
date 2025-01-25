f631e05caf7763b42434757abf219d38ccebc9d3
Diya k Sajith <diyaksajith@gmail.com>
2025-01-26 02:51:29 +0530

    footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -24,6 +24,7 @@ function App() {
         <Route path='/cart' element={<Cart/>}/>
         <Route path='/login' element={<Signup/>}/>
       </Routes>
+      <footer/>
       </BrowserRouter>
       
     </div>
