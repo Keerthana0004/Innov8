c00a2001386623820e9f99bd6d1510b55d793d37
Keerthana0004 <keerthanasivan04@gmail.com>
2025-01-26 00:24:59 +0530

    Home

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,10 @@
 // for navigation bar
 
-import React, {useState} from 'react' 
-import './Navbar.css'
-import logo from '../Assets/logo.jpg'
-import cart from '../Assets/cart.png'
+import React, {useState} from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+import logo from '../Assets/logo.jpg';
+import cart from '../Assets/cart.png';
 const Navbar = () => {
 
     const [menu,setMenu] = useState("shop");
@@ -16,14 +17,14 @@ const Navbar = () => {
                 <p>STYLENOOK</p>
             </div>
             <ul className="nav-menu">
-                <li onClick={()=>{setMenu("shop")}}>Shop {menu==="shop"?<hr/>:<></>}</li>
-                <li onClick={()=>{setMenu("mens")}}>Men  {menu==="mens"?<hr/>:<></>}</li>
-                <li onClick={()=>{setMenu("women")}}>Women {menu==="women"?<hr/>:<></>}</li>
-                <li onClick={()=>{setMenu("kids")}}>Kids {menu==="kids"?<hr/>:<></>}</li>
+                <li onClick={()=>{setMenu("shop")}}> <Link to='/'>Shop </Link>style={{ textDecoration: 'none'}}{menu==="shop"?<hr/>:<></>}</li>
+                <li onClick={()=>{setMenu("mens")}}><Link to='/mens'>Men </Link>style={{ textDecoration: 'none'}} {menu==="mens"?<hr/>:<></>}</li>
+                <li onClick={()=>{setMenu("women")}}><Link to='/women'>Women </Link>style={{ textDecoration: 'none'}}{menu==="women"?<hr/>:<></>}</li>
+                <li onClick={()=>{setMenu("kids")}}><Link to='/kids'>Kids </Link> style={{ textDecoration: 'none'}}{menu==="kids"?<hr/>:<></>}</li>
             </ul>
             <div className="nav-login-cart">        
-                <button>Login</button>
-                <img src={cart} alt="" style={{ width: '70px', height: 'auto'}}/>
+                <Link to='/login'><button>Login</button></Link>
+                <Link to='/cart'><img src={cart} alt="" style={{ width: '70px', height: 'auto'}}/></Link>
                 <div className="nav-cart-count">
                 0
                 </div>
