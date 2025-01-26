9521c45ac8f92f4a5a02bee92fd9f62e446f60b9
Keerthana0004 <keerthanasivan04@gmail.com>
2025-01-26 11:02:22 +0530

    screenrecording

---
+++ backend/public/images/data.js
@@ -0,0 +1,41 @@
+import p1_img from './men shirt1.jpg'
+import p2_img from './women top1.jpg'
+import p3_img from './men pant2.jpg'
+import p4_img from './kid kurti2.jpg'
+    let data_product= [
+      {
+        id: 1,
+        name: "Men's Casual Shirt",
+        category: "men",
+        image: p1_img,
+        newPrice: 199.99,
+        oldPrice: 299.99
+      },
+      {
+        id: 2,
+        name: "Women's trending top",
+        category: "women",
+        image: p2_img,
+        newPrice: 344.99,
+        oldPrice: 499.99
+      },
+      {
+        id: 3,
+        name: "Men's popular pants",
+        category: "men",
+        image: p3_img,
+        newPrice: 445.99,
+        oldPrice: 595.99
+      },
+      {
+        id: 4,
+        name: "Kids trending kurti",
+        category:"kid",
+        image: p4_img,
+        newPrice: 244.99,
+        oldPrice: 344.99
+      },
+      
+    
+    ];
+    export default data_product;
